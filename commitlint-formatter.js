6db8579b404c770600cb14c79e9ac370f9b44398
NTQ-t2309274 <t2309274@partner.coway.com>
2025-05-30 14:44:33 +0700

    feat(config): update config

--- commitlint-formatter.js
+++ commitlint-formatter.js
@@ -13,7 +13,7 @@ module.exports = (report) => {
       result.errors.map((err) => ({
         file: result.input,
         message: err.message,
-      }))
+      })),
     )
     .flat();
 
--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,27 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'scope-enum': [2, 'always', ['config', 'publishing', 'tms', 'pnt', 'partners']],
+    'scope-enum': [
+      2,
+      'always',
+      ['config', 'publishing', 'tms', 'pnt', 'partners'],
+    ],
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'test', 'modify'],
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'modify',
+      ],
     ],
   },
-}
+};
